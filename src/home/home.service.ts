7393d9b92e6c524942919a95a5b052685309eeaf
Pakorn Sukmankong <pakorn.su@Pakorns-MacBook-Pro.local>
2022-12-27 10:28:06 +0700

    feat:query home

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomesResponseDto } from './dto/home.dto';
+import { GetHomesParams } from './interface/home.interface';
 
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getHomes(): Promise<HomesResponseDto[]> {
+  async getHomes(filter: GetHomesParams): Promise<HomesResponseDto[]> {
     const homes = await this.prismaService.home.findMany({
       select: {
         id: true,
@@ -15,18 +16,24 @@ export class HomeService {
         price: true,
         propertyType: true,
         number_of_bathrooms: true,
-        number_of_bedroom: true,
-        Images: {
+        number_of_bedrooms: true,
+        images: {
           select: {
             url: true,
           },
           take: 1,
         },
       },
+      where: filter,
     });
+
+    if (!homes.length) {
+      throw new NotFoundException('Home Not Found');
+    }
+
     return homes.map((home) => {
-      const fetchHome = { ...home, image: home.Images[0].url };
-      delete fetchHome.Images;
+      const fetchHome = { ...home, image: home.images[0].url };
+      delete fetchHome.images;
       return new HomesResponseDto(fetchHome);
     });
   }
